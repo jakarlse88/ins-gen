b4428d20af0077f419e7769b195879041f13d366
Jon Karlsen <karlsen.jonarild@gmail.com>
2019-02-15 12:47:04 +0100

    Complete prototype

---
+++ js/index.js
@@ -0,0 +1,70 @@
+const dictionary = {
+	adjectives: [
+		"abominable",
+		"reprehensible",
+		"sorry",
+		"intolerable",
+		"illiterate",
+		"troublesome",
+		"undeserving"
+	],
+	expletives: [
+		"shit",
+		"fuck",
+		"excuse",
+		"ass",
+		"twat",
+		"cunt",
+		"goddamn",
+		"damn"
+	],
+	nouns: [
+		"goblin",
+		"waffle",
+		"excuse",
+		"pastry",
+		"truck",
+		"elbow",
+		"plum",
+		"llama"
+	]
+};
+
+/**
+ * Generate a new word for each field and update document.
+ */
+const onGenerateBtnClick = () => {
+	const elements = {
+		adjectives: "#adjective-field",
+		expletives: "#expletive-field",
+		nouns: "#noun-field"
+	};
+
+	Object.keys(dictionary).forEach(key => {
+		_updateWord(dictionary[key], elements[key]);
+	});
+};
+
+/**
+ * Events to be handled
+ */
+const callback = () => {
+	document
+		.querySelector("#generate-btn")
+		.addEventListener("click", onGenerateBtnClick);
+};
+
+/**
+ * Register events on document ready
+ */
+if (
+	document.readyState === "complete" ||
+	(document.readyState === "loading" &&
+		!document.documentElement.doScroll)
+) {
+	callback();
+} else {
+	document.addEventListener("DOMContentLoaded", () =>
+		callback()
+	);
+}
---
+++ js/util.js
@@ -0,0 +1,39 @@
+/**
+ * Returns a random integer between min (inclusive) and max (exclusive).
+ */
+const _getRandomInt = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return (
+		Math.floor(Math.random() * (max - min)) + min
+	);
+};
+
+/**
+ * Returns a random element from a given array.
+ */
+const _getRandomElement = arr => {
+	const max = arr.length;
+	const index = _getRandomInt(0, max);
+
+	return arr[index];
+};
+
+/**
+ * Randomly chooses a new word and updates the document.
+ */
+const _updateWord = (arr, elem) => {
+	// Create a new node from a randomly chosen string
+	const newNode = document.createTextNode(
+		_getRandomElement(arr)
+	);
+
+	// Find element in document
+	const wordField = document.querySelector(elem);
+
+	// Remove element's first child
+	wordField.removeChild(wordField.childNodes[0]);
+
+	// Append textNode
+	wordField.appendChild(newNode);
+};
